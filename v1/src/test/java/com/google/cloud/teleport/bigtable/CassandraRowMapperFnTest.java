cfbc14d782025d689ad121e56d7cf5692106a4d3
vihaan rao <63618681+vihaanrao@users.noreply.github.com>
2023-03-26 04:31:01 -0500

    Update CassandraRowMapperFnTest.java

--- v1/src/test/java/com/google/cloud/teleport/bigtable/CassandraRowMapperFnTest.java
+++ v1/src/test/java/com/google/cloud/teleport/bigtable/CassandraRowMapperFnTest.java
@@ -141,13 +141,13 @@ public class CassandraRowMapperFnTest extends CassandraBaseTest {
 
     Schema schema = Schema.builder().addNullableField("col", FieldType.FLOAT).build();
     Row expected = Row.withSchema(schema).addValue(value).build();
-
+    
     // Map the resultSet to Apache Beam Row using the cassandraRowMapper
     Row resultRow = cassandraRowMapper.map(resultSet).next();
 
     // Compare float values with a small tolerance
     float delta = 1e-6f;
-    assertEquals(expected.getValue("col"), resultRow.getValue("col"), delta);
+    assertEquals((float) expected.getValue("col"), (float) resultRow.getValue("col"), delta);
   }
 
   @Test
